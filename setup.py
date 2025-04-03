59bb42f7489a7dc8a56da32957ddd2e0390f512f
omkar-pomendkar <omkarpomendkar100@gmail.com>
2025-04-04 01:30:58 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Omkar Pomendkar',
+    author_email= 'omkarpomendkar100@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
